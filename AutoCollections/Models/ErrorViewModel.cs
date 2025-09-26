f9fa91a24647d211bf20474b63626f5da9bba124
JuS0l <ju.s.oliveira23@gmail.com>
2025-09-26 16:18:19 -0300

    Iniciando o projeto e criando o .gitignore.

---
+++ AutoCollections/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AutoCollections.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
